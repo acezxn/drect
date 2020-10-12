b776ce382abb05429c428a3c87a6e7bed4731e5f
acezxn <daniel@Macintosh-9.local>
2020-10-12 09:18:23 +0800

    make it better

--- routes/mainpage.js
+++ routes/mainpage.js
@@ -3,27 +3,28 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 var mongo = require('mongodb');
 var MongoClient = mongo.MongoClient;
-var url = "mongodb://localhost:27017/db";
+// var url = "mongodb://localhost:27017/db";
+var user_url = "mongodb+srv://user:Daniel@cluster0.2k6zl.mongodb.net/<dbname>?retryWrites=true&w=majority"
 router.use(bodyParser.urlencoded({extended: false}));
 router.use(bodyParser.json());
-MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
-  if (err) throw err;
-  var dbo = db.db('db');
-  dbo.createCollection("urls", function(err, res) {
-    // if (err) throw err;
-    console.log("Collection created!");
-    db.close();
-  });
-});
-router.get('/', (req, res, next) => {
-  res.status(200).json({
-    results: 'the mainpage'
-  });
-
-});
+// MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+//   if (err) throw err;
+//   var dbo = db.db('db');
+//   dbo.createCollection("urls", function(err, res) {
+//     // if (err) throw err;
+//     console.log("Collection created!");
+//     db.close();
+//   });
+// });
+// router.get('/', (req, res, next) => {
+//   res.status(200).json({
+//     results: 'the mainpage'
+//   });
+//
+// });
 router.get('/:attb', (req, res, next) => {
   const id = req.params.attb;
-  MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
+  MongoClient.connect(user_url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
   // if (err) throw err;
   var dbo = db.db("db");
   dbo.collection("urls").find({"name": id}).toArray( function(err, result) {
@@ -42,13 +43,16 @@ router.get('/:attb', (req, res, next) => {
 });
 });
 
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: "welcome to mainpage"
+  });
+});
+
 router.put('/', (req, res, next) => {
   console.log(req.body);
-  if (req.body.user == "Daniel" && req.body.passwd == "Daniel") {
-    const product = {
-      name: req.body.name,
-      number: req.body.number
-    };
+  var url = "mongodb+srv://" + req.body.user +":" + req.body.passwd + "@cluster0.2k6zl.mongodb.net/<dbname>?retryWrites=true&w=majority"
+  console.log(url);
 
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
     if (err) throw err;
@@ -63,13 +67,14 @@ router.put('/', (req, res, next) => {
 
     res.status(200).json({
       message: 'put',
-      prod: product
+      name: req.body.name,
+      url: req.body.url
     });
-  } else {
-    res.status(200).json({
-      error: 'Authorization failed'
-    });
-  }
+  // } else {
+  //   res.status(200).json({
+  //     error: 'Authorization failed'
+  //   });
+  // }
 
 });
 
